4f77a3398e28d6dbeabb41565796540f8e0f3698
alvintnw <alvin.tnw@gmail.com>
2026-05-22 14:44:43 +0700

    Remove sidebar user panel
    Removed user panel from sidebar to bring menu items up.

--- template/sidebar.php
+++ template/sidebar.php
@@ -4,23 +4,12 @@
     <a href="<?= $main_url ?>dashboard.php" class="brand-link">
         <img src="<?= $main_url ?>asset/image/logo.png" alt="Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">Kasir</span>
+        <span class="brand-text font-weight-light">Wartech</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="<?= $main_url ?>asset/image/<?= userLogin()['foto'] ?>" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
-
-
-
+        <!-- Sidebar user panel removed to bring menu items up -->
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
